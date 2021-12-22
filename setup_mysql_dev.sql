48866e537c0be7f8b42ef65cc82978a38cd4de37
brk-a <fnjakai@outlook.com>
2021-12-22 05:32:20 -0800

    Tasks 0 - 10: try v 1.2

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- create a database and User
+-- Grant a user select priv for performance_schema
+-- Should not fail if database exists
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
