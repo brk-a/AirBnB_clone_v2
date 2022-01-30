c47726fb423201e071f57b58485bf6287802ba35
brk-a <fnjakai@outlook.com>
2022-01-30 03:58:45 -0800

    Tasks 8 and 9: try v 1.0

---
+++ web_flask/9-states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+Show states and cities by state views
+"""
+
+from flask import Flask, render_template, g
+import models
+from models.state import State
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/states', strict_slashes=False)
+def liststates():
+    """view that lists all of the states"""
+    id = None
+    states = models.storage.all(State).values()
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def liststatecities(id=None):
+    """view that lists all cities by their states"""
+    found = False
+    states = models.storage.all(State).values()
+    for state in states:
+        if state.id == id:
+            found = True
+            states = [state]
+    if found is False:
+        states = []
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """remove the current SQLAlchemy Session"""
+    models.storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
